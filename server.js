2a7fbf77d094223de70e7ae585b82e1d050e9044
Clara <clarxyg@gmail.com>
2023-10-18 14:54:49 -0300

    feat(ssr): add babelrc and express configuration for server-side-rendering

---
+++ server.js
@@ -0,0 +1,30 @@
+require('@babel/register')({
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+});
+
+
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const App = require('./src/App');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const html = ReactDOMServer.renderToString(App);
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Elegance Boutique</title>
+    </head>
+    <body>
+      <div id="root">${html}</div>
+    </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor está rodando na porta 3000');
+});
